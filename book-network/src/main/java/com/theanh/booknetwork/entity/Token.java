4b426ecdd0aa3704c406d3f887f58d930e43c4de
AnhNT <anhntse183225@fpt.edu.vn>
2024-12-21 20:05:55 +0700

    Added Spring Security (deprecated)

---
+++ book-network/src/main/java/com/theanh/booknetwork/entity/Token.java
@@ -0,0 +1,31 @@
+package com.theanh.booknetwork.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    String token;
+
+    LocalDateTime createdAt;
+
+    LocalDateTime expiresAt;
+
+    LocalDateTime validatedAt;
+
+    @ManyToOne
+    User user;
+}
